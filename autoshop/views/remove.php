2ce1175051165deab772e61b569b4adebf88210b
Nik4m3 <nik4m3@mail.ru>
2017-02-10 15:39:03 +0500

    Update remove.php

--- autoshop/views/remove.php
+++ autoshop/views/remove.php
@@ -1,14 +1,11 @@
 <?php
 echo '$data:' . "<br>";
-var_dump($data);
 echo "<br><br>";
 echo '$userData:' . "<br>";
-var_dump($userData);
 echo "<br><br>";
 extract($result);
 echo "<br><br>";
 echo '$result:' . "<br>";
-var_dump($result);
 ?>
 <div>
 
